aa9cbdf6abc6570bf5588f3e973a9be87584fbc5
7719Drinkin <drinkinhuang@gmail.com>
2026-07-30 18:03:55 +0800

    fix: rebuild 23 monument with safe athletic number geometry

--- apps/solar-universe/src/basketball/Number23Monument.jsx
+++ apps/solar-universe/src/basketball/Number23Monument.jsx
@@ -11,46 +11,54 @@ function shapeFrom(points) {
   return shape;
 }
 
+// Block-athletic silhouettes with no self-intersections. The extra side bearing
+// prevents the left edge of the 2 from being cut by the plaque frame in perspective.
 const DIGIT_2 = [
-  [-0.30, 0.50], [0.15, 0.50], [0.29, 0.39], [0.29, 0.19], [0.20, 0.08],
-  [-0.08, -0.12], [-0.25, -0.27], [-0.25, -0.50], [0.30, -0.50], [0.30, -0.28],
-  [-0.03, -0.28], [0.10, -0.17], [0.28, -0.02], [0.30, 0.08], [0.30, 0.32],
-  [0.16, 0.48], [-0.30, 0.48]
+  [-0.34, 0.50], [0.15, 0.50], [0.34, 0.37], [0.34, 0.12], [0.24, -0.02],
+  [-0.04, -0.23], [-0.15, -0.32], [0.34, -0.32], [0.34, -0.50], [-0.34, -0.50],
+  [-0.34, -0.27], [-0.23, -0.12], [0.04, 0.08], [0.13, 0.17], [0.13, 0.29],
+  [0.06, 0.34], [-0.34, 0.34]
 ];
 
 const DIGIT_3 = [
-  [-0.30, 0.50], [0.13, 0.50], [0.29, 0.38], [0.29, 0.15], [0.20, 0.04],
-  [0.29, -0.07], [0.29, -0.38], [0.13, -0.50], [-0.30, -0.50], [-0.30, -0.28],
-  [0.04, -0.28], [0.10, -0.23], [0.10, -0.11], [0.04, -0.06], [-0.16, -0.06],
-  [-0.16, 0.11], [0.04, 0.11], [0.10, 0.17], [0.10, 0.25], [0.04, 0.30],
-  [-0.30, 0.30]
+  [-0.34, 0.50], [0.13, 0.50], [0.33, 0.38], [0.33, 0.12], [0.22, 0.01],
+  [0.33, -0.10], [0.33, -0.38], [0.13, -0.50], [-0.34, -0.50], [-0.34, -0.32],
+  [0.03, -0.32], [0.12, -0.26], [0.12, -0.13], [0.04, -0.07], [-0.17, -0.07],
+  [-0.17, 0.10], [0.04, 0.10], [0.12, 0.17], [0.12, 0.27], [0.04, 0.33],
+  [-0.34, 0.33]
 ];
 
 function Digit({ points, x }) {
   const geometry = useMemo(() => {
-    const shape = shapeFrom(points);
-    const result = new THREE.ExtrudeGeometry(shape, {
-      depth: 0.075,
+    const result = new THREE.ExtrudeGeometry(shapeFrom(points), {
+      depth: 0.082,
       bevelEnabled: true,
-      bevelSegments: 2,
+      bevelSegments: 3,
       bevelSize: 0.018,
-      bevelThickness: 0.014,
+      bevelThickness: 0.016,
       curveSegments: 4
     });
     result.center();
+    result.computeVertexNormals();
     return result;
   }, [points]);
 
   return (
     <group position={[x, 0, 0]}>
-      <mesh geometry={geometry} scale={[1.18, 1.18, 1]} position-z={-0.045}>
-        <meshStandardMaterial color="#090909" roughness={0.68} metalness={0.12} />
+      <mesh geometry={geometry} scale={[1.2, 1.2, 1]} position-z={-0.055}>
+        <meshStandardMaterial color="#050506" roughness={0.7} metalness={0.1} />
       </mesh>
-      <mesh geometry={geometry} scale={[1.09, 1.09, 1]} position-z={-0.01}>
-        <meshStandardMaterial color="#f1eee7" roughness={0.62} metalness={0.04} />
+      <mesh geometry={geometry} scale={[1.1, 1.1, 1]} position-z={0.005}>
+        <meshStandardMaterial color="#f4f0e8" roughness={0.58} metalness={0.03} />
       </mesh>
-      <mesh geometry={geometry} position-z={0.035}>
-        <meshStandardMaterial color="#c70f2e" emissive="#3b0010" emissiveIntensity={0.18} roughness={0.55} metalness={0.08} />
+      <mesh geometry={geometry} position-z={0.07}>
+        <meshStandardMaterial
+          color="#d31132"
+          emissive="#3d0010"
+          emissiveIntensity={0.16}
+          roughness={0.5}
+          metalness={0.09}
+        />
       </mesh>
     </group>
   );
@@ -58,28 +66,28 @@ function Digit({ points, x }) {
 
 export default function Number23Monument({ radius }) {
   return (
-    <group position={[0, -0.07, radius * 0.96]} scale={0.78}>
-      <mesh position-z={-0.09}>
-        <boxGeometry args={[1.45, 1.08, 0.12, 4, 4, 1]} />
-        <meshStandardMaterial color="#241a16" roughness={0.96} metalness={0} />
+    <group position={[0, -0.06, radius * 0.965]} scale={0.76}>
+      <mesh position-z={-0.115}>
+        <boxGeometry args={[1.72, 1.18, 0.16, 4, 4, 1]} />
+        <meshStandardMaterial color="#2c201a" roughness={0.96} metalness={0} />
       </mesh>
-      <mesh position-z={-0.02}>
-        <boxGeometry args={[1.34, 0.98, 0.035]} />
-        <meshStandardMaterial color="#060607" roughness={0.82} metalness={0.12} />
+      <mesh position-z={-0.035}>
+        <boxGeometry args={[1.58, 1.06, 0.055]} />
+        <meshStandardMaterial color="#050507" roughness={0.78} metalness={0.12} />
       </mesh>
       <mesh position-z={0.005}>
-        <boxGeometry args={[1.27, 0.91, 0.018]} />
-        <meshBasicMaterial color="#8f0c25" />
+        <boxGeometry args={[1.48, 0.98, 0.022]} />
+        <meshBasicMaterial color="#a10b25" />
       </mesh>
-      <mesh position-z={0.025}>
-        <boxGeometry args={[1.22, 0.86, 0.015]} />
+      <mesh position-z={0.036}>
+        <boxGeometry args={[1.39, 0.9, 0.018]} />
         <meshBasicMaterial color="#050506" />
       </mesh>
-      <group position-z={0.12} scale={0.82}>
-        <Digit points={DIGIT_2} x={-0.35} />
-        <Digit points={DIGIT_3} x={0.35} />
+      <group position={[0.015, 0, 0.145]} scale={0.76}>
+        <Digit points={DIGIT_2} x={-0.39} />
+        <Digit points={DIGIT_3} x={0.39} />
       </group>
-      <pointLight position={[0, 0.05, 0.42]} color="#e53a43" intensity={1.2} distance={2.1} decay={2} />
+      <pointLight position={[0, 0.04, 0.5]} color="#e83043" intensity={0.75} distance={1.8} decay={2} />
     </group>
   );
 }
