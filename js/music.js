12793dff87b088a674d2fb902912c5678bfee78b
7719Drinkin <drinkinhuang@gmail.com>
2026-08-06 13:52:59 +0800

    build: refresh Music catalog asset version

--- js/music.js
+++ js/music.js
@@ -42,7 +42,7 @@ if (catalogPage === 'album') {
 
 if (catalogPage && !document.querySelector('script[data-music-catalog-loader]')) {
   const script = document.createElement('script');
-  script.src = '/js/music-catalog.js?v=20260806-albums-2';
+  script.src = '/js/music-catalog.js?v=20260806-albums-3';
   script.dataset.musicCatalogLoader = '';
   document.body.append(script);
 }
